daaf4bc08ec578e162f800be957c5fd1e5049a3c
Kalmai221 Plays <67641154+Kalmai221@users.noreply.github.com>
2021-07-07 22:10:57 +0100

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,5 @@
+console.log("This website was created by Kalmai221 Plays. Please do not take the website Source Code and use it for your own purposes. You must respect the World Wide Web and be nice to people who just want to teach others such as Kalmai221 Plays for trying to teach YOU how to code!")
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
